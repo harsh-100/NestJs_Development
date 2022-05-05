0b07fc6d14f60af01abb55743743b5145b551f84
harsh-100 <26harshagarwal@gmail.com>
2022-05-05 18:44:46 +0530

    initital commit

---
+++ Creating_API/src/products/product.model.ts
@@ -0,0 +1,11 @@
+export class Product {
+
+//we are adding the comment
+
+    constructor(
+        public id:string,
+        public title:string,
+         public description:string,
+         public price:number )
+{ };
+}
